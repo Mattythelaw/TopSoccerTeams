0177fd17dea45c01d4e6a06cfa97fc5e6bf8722d
Matthew Lawrence <matthewconnor0418@gmail.com>
2026-04-15 15:58:01 +0200

    Updated UI changed background

--- app/src/main/java/com/example/topsoccerteams/MainActivity.kt
+++ app/src/main/java/com/example/topsoccerteams/MainActivity.kt
@@ -9,20 +9,6 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import java.util.Arrays
 
-//created function to display in the terminal
-fun logArrValues(arr: Array<String>, limit:  Int = 0) {
-    //if statement to check if there is a limit//
-    if (limit == 0) {
-        Log.v("Array Values: ", Arrays.toString(arr))
-    } else {
-        Log.v(
-            "Array values: ", Arrays.toString(
-                arr.sliceArray
-                    (0..limit - 1)
-            )
-        )
-    }
-}
 class MainActivity : AppCompatActivity() {
 
     val teams = arrayOf("Liverpool", "Man City", "Manchester", "Orlando Pirates", "Chelsea")
@@ -36,27 +22,22 @@ class MainActivity : AppCompatActivity() {
         var teamDisplayTxt = " "
         var count = 0
 
-        //Calling logArrValues function
-        logArrValues(teams)
-        logArrValues(teams, 4)
-
         //reassigning position 0 of the teams array//
         teams[0] = "Liverpool :)"
 
-        //while loop to iterate through the teams array and display team//
-        //       while(count < teams.count()){
-        //           teamDisplayTxt += "${teams[count]}\n"
-        //           count++
-        //       }
+        //while loop to iterate through the teams array and display team
+               while(count < teams.count()){
+                   teamDisplayTxt += "${teams[count]}\n"
+                 count++
+             }
 
-        for (team in teams) {
-            teamDisplayTxt += "${teams}\n"
-        }
-
-
+//        for (team in teams) {
+//            teamDisplayTxt += "${teams}\n"//
+//        }
 
 
-        teamsTxt.text = Arrays.toString(teams)
+
+        teamsTxt.text = teamDisplayTxt;
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
